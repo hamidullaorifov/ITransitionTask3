bb3f72277a57c3fd6b9d90659c2fdb6a7e165778
Khamidulla <khamidulla_orifov@epam.com>
2025-09-24 10:45:14 +0500

    Init

---
+++ ITransitionTask3/Controllers/LcmController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITransitionTask3.Controllers;
+
+[Route("orifovhamidulla_gmail_com")]
+[ApiController]
+public class LcmController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get([FromQuery] string? x, [FromQuery] string? y)
+    {
+        if (!int.TryParse(x, out int a) || !int.TryParse(y, out int b) || a <= 0 || b <= 0)
+        {
+            return Content("NaN", "text/plain");
+        }
+
+        int gcd = Gcd(a, b);
+        long lcm = (long)a / gcd * b; // use long to avoid overflow
+        return Content(lcm.ToString(), "text/plain");
+    }
+
+    private int Gcd(int a, int b)
+    {
+        while (b != 0)
+        {
+            int t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
+    }
+}
